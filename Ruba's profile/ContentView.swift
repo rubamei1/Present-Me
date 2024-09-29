7eafce646e96974680f9e4e0069937743dc7dc46
rubamei1 <rubameshal12@hotmail.com>
2024-09-29 10:26:32 +0300

    Present Me

--- Ruba's profile/ContentView.swift
+++ Ruba's profile/ContentView.swift
@@ -9,12 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack (alignment:.leading) {
+            Image("RUBY1")
+                .resizable() . frame(width:200, height:200)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.blue,lineWidth: 8))
+                .aspectRatio(contentMode: .fit)
+                
+                .padding()
+            Text("Ruba Alqahtani")
+                .font(.title)
+                .foregroundColor(.gray)
+            Text("An ambitious translator/interpreter, I can speak three languages, learning new things is my passion, I thrive in Cooaperative environments")
+                .foregroundColor(.blue)
         }
+        .padding()
         .padding()
     }
 }
